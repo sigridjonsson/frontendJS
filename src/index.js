b4df4e5bec512f33a42aba0e8e47ae2f95dd645a
Sigrid Jönsson <sigridjonsson@Sigrids-MacBook-Pro.local>
2021-09-01 13:27:49 +0200

    Added a TinyMCE text editor

--- src/index.js
+++ src/index.js
@@ -1,78 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-
-class Square extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          value: null,
-      };
-  }
-
-  render() {
-    return (
-      <button
-      className="square"
-      onClick={() => this.setState({value: 'X'})}
-      >
-        {this.state.value}
-      </button>
-    );
-  }
+export default function App() {
+  const editorRef = useRef(null);
+  const log = () => {
+    if (editorRef.current) {
+      let withTags = editorRef.current.getContent();
+      let withOneTag = withTags.replace('<p>', '');
+      console.log(withOneTag.replace('</p>', ''));
+    }
+  };
+  return (
+    <>
+    <div className="toolbar">
+        <button className="save" onClick={log}>Spara</button>
+    </div>
+      <Editor
+        onInit={(evt, editor) => editorRef.current = editor}
+        initialValue=""
+        init={{
+          height: 400,
+          menubar: false,
+          plugins: [
+            'advlist autolink lists link image charmap print preview anchor',
+            'searchreplace visualblocks code fullscreen',
+            'insertdatetime media table paste code help wordcount'
+          ],
+          toolbar: 'undo redo | formatselect | ' +
+          'bold italic backcolor | alignleft aligncenter ' +
+          'alignright alignjustify | bullist numlist outdent indent | ' +
+          'removeformat | help',
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+        }}
+      />
+    </>
+  );
 }
 
-class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
-  }
-
-  render() {
-    const status = 'Next player: X';
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
-
-class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
-}
-
-// ========================================
 
 ReactDOM.render(
-  <Game />,
+  <App />,
   document.getElementById('root')
 );
