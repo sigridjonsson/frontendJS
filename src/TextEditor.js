2af4b44824ecc480aa5359328f5aaabcb6cb9922
Sigrid Jönsson <sigridjonsson@Sigrids-MacBook-Pro.local>
2021-09-12 13:21:35 +0200

    Moved GetDocuments

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,27 +1,45 @@
-import React, { useRef } from 'react';
+import { React, useRef, useState, useEffect } from 'react';
 import { Editor } from '@tinymce/tinymce-react';
-import GetDocuments from './Documents.js';
 
 export default function TextEditor() {
   const editorRef = useRef(null);
   const log = () => {
     if (editorRef.current) {
-      let withTags = editorRef.current.getContent();
-      let withOneTag = withTags.replace('<p>', '');
-      console.log(withOneTag.replace('</p>', ''));
+      let text = editorRef.current.getContent();
+      console.log(text);
   }
+  };
+  const save = (e) => {
+      if (editorRef.current || e.target[0].value) {
+          e.preventDefault();
+          fetch('http://localhost:1337/documents', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded',
+              },
+              body: JSON.stringify({
+                  name: e.target[0].value,
+                  text: editorRef.current.getContent()
+              })
+          })
+          .then( function (response) {
+              alert(response);
+              // console.log(response);
+          });
+    }
   };
   return (
     <>
     <div className="toolbar">
         <button className="log" onClick={log}>Logga</button>
     </div>
-    <form>
+    <form onSubmit={save}>
         <label>Namn på dokument</label><br />
-        <input type="text" id="name" />
+        <input type="text" id="nameDoc" />
         <input type="submit" value="Spara" />
     </form>
       <Editor
+        id="myTextarea"
         onInit={(evt, editor) => editorRef.current = editor}
         initialValue=""
         init={{
@@ -39,7 +57,33 @@ export default function TextEditor() {
           content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
         }}
       />
-      <GetDocuments />
+      < GetDocuments />
     </>
   );
+}
+
+
+function GetDocuments() {
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        fetch("http://localhost:1337/documents")
+        .then(res => res.json())
+        .then(data => setData(data))
+    }, [])
+
+    let allDocs = [];
+
+    for (let i = 0; i < data.length; i++) {
+        allDocs.push(<div onClick={() => doSomething(data[i].text)}>{data[i].name}</div>);
+    }
+
+    return (
+        allDocs
+    )
+}
+
+
+function doSomething(text) {
+    console.log(text);
 }
