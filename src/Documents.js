0263aaf8edd63e738c26e43d8bd8ec213d23ee87
Sigrid Jönsson <sigridjonsson@Sigrids-MacBook-Pro.local>
2021-10-04 15:40:58 +0200

    First version for AUTH

--- src/Documents.js
+++ src/Documents.js
@@ -2,20 +2,41 @@ import React, { useState, useEffect } from 'react';
 
 export default function Documents({ onTitleClick }) {
     const [data, setData] = useState({})
-
+    const headers = { 'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNpZ3JpZEBzaWdyaWQuc2UiLCJpYXQiOjE2MzMzNDc5MjUsImV4cCI6MTYzMzM1MTUyNX0.aqtuoroWqdKNrTlMI46k56TLYd5sPi-j8qslsJVXvK0'}
     useEffect(() => {
-        fetch("https://jsramverk-editor-sijn20.azurewebsites.net/documents")
+        fetch("http://localhost:1337/documents", { headers })
         .then(res => res.json())
         .then(data => setData(data))
     }, [])
+    // console.log(data);
 
-    let allDocs = [];
+    if (data.length > 0) {
+        let allDocs = [];
 
-    for (let i = 0; i < data.length; i++) {
-        allDocs.push(<div className="indDoc" onClick={() => onTitleClick(data[i]._id, data[i].name, data[i].text)}>{data[i].name}</div>);
-    }
+        for (let i = 0; i < data.length; i++) {
+            allDocs.push(<div className="indDoc" onClick={() => onTitleClick(data[i]._id, data[i].name, data[i].text)}>{data[i].name}</div>);
+        }
 
-    return (
-        allDocs
-    )
-}
+        return (
+            allDocs
+        )
+    } 
+    // console.log(data.length);
+    return "Inga dokument"
+
+    // fetch(`https://jsramverk-editor-sijn20.azurewebsites.net/documents`)
+    // .then(function (response) {
+    //     // console.log(response);
+    //     return response.json();
+    // }).then(function(data) {
+    //     console.log(data);
+    //     let allDocs = [];
+
+    //     for (let i = 0; i < data.length; i++) {
+    //         allDocs.push(<div className="indDoc" onClick={() => onTitleClick(data[i]._id, data[i].name, data[i].text)}>{data[i].name}</div>);
+    //     }
+    //     console.log("alldocs", allDocs);
+
+    //     return allDocs;
+    // });
+};
--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,7 +1,8 @@
-import { React, useRef } from 'react';
+import React from 'react';
 import { Editor } from '@tinymce/tinymce-react';
 import Toolbar from './Toolbar.js';
 import Documents from './Documents.js';
+import FirstPage from './FirstPage.js';
 
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "https://jsramverk-editor-sijn20.azurewebsites.net/";
@@ -9,42 +10,51 @@ const ENDPOINT = "https://jsramverk-editor-sijn20.azurewebsites.net/";
 const socket = socketIOClient(ENDPOINT);
 
 
-export default function TextEditor() {
-  const editorRef = useRef(null);
-  const log = () => {
-    if (editorRef.current) {
-      let text = editorRef.current.getContent();
+export default class TextEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.editorRef = React.createRef();
+    this.state = {
+        token: null,
+        email: null
+    };
+  }
+  log = () => {
+    if (this.editorRef.current) {
+      let text = this.editorRef.current.getContent();
       console.log(text);
     }
   };
-  const empty = () => {
-    if (editorRef.current) {
+  empty = () => {
+    if (this.editorRef.current) {
       const inputId = document.getElementById('idDoc');
       inputId.value = "";
       const inputName = document.getElementById('nameDoc');
       inputName.value = "";
-      editorRef.current.setContent("");
+      this.editorRef.current.setContent("");
     }
   };
-  const save = (e) => {
+  save = (e) => {
       if (e.target[1].value) {
           e.preventDefault();
-          fetch(`https://jsramverk-editor-sijn20.azurewebsites.net/documents/${e.target[1].value}`, {
+          fetch(`http://localhost:1337/documents/${e.target[1].value}`, {
               method: 'PUT',
               headers: {
                   'Accept': 'application/json',
                   'Content-Type': 'application/json',
+                  'x-access-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNpZ3JpZEBzaWdyaWQuc2UiLCJpYXQiOjE2MzMzNTE3ODYsImV4cCI6MTYzMzM1NTM4Nn0.QnDQ4_DjLehT3CsWdN98mPCJlMt_MCXf82jnzeHtrcQ'
               },
               body: JSON.stringify({
                   name: e.target[0].value,
-                  text: editorRef.current.getContent(),
+                  text: this.editorRef.current.getContent(),
               })
           });
           alert("Uppdaterad!");
       }
       else {
+        console.log(e.target[2].value);
           e.preventDefault();
-          fetch('https://jsramverk-editor-sijn20.azurewebsites.net/documents', {
+          fetch('http://localhost:1337/documents', {
               method: 'POST',
               headers: {
                   'Accept': 'application/json',
@@ -52,7 +62,11 @@ export default function TextEditor() {
               },
               body: JSON.stringify({
                   name: e.target[0].value,
-                  text: editorRef.current.getContent(),
+                  text: this.editorRef.current.getContent(),
+                  owner: "sigrid@sigrid.se",
+                  allowed_users: [
+                    e.target[2].value,
+                  ],
               })
           })
           .then( function (response) {
@@ -60,53 +74,99 @@ export default function TextEditor() {
           });
       };
   };
-  function showText(docId, name, text) {
-      const inputId = document.getElementById('idDoc');
-      inputId.value = docId;
-      const inputName = document.getElementById('nameDoc');
-      inputName.value = name;
-      editorRef.current.setContent(text);
-      socket.emit("create", docId);
-  };
-  const live = () => {
+  showText = (docId, name, text) => {
+        const inputId = document.getElementById('idDoc');
+        inputId.value = docId;
+        const inputName = document.getElementById('nameDoc');
+        inputName.value = name;
+        this.editorRef.current.setContent(text);
+        socket.emit("create", docId);
+    };
+  live = () => {
     socket.on("doc", (data) => {
-      editorRef.current.setContent(data.html);
+      this.editorRef.current.setContent(data.html);
     });
 
     const inputId = document.getElementById('idDoc');
     let data = {
         _id: inputId.value,
-        html: editorRef.current.getContent()
+        html: this.editorRef.current.getContent()
     };
     socket.emit("doc", data);
   };
-  return (
-    <>
-    < Toolbar saveText={save} logText={log} emptyEditor={empty} />
-      <Editor
-        id="myTextarea"
-        onInit={(evt, editor) => editorRef.current = editor}
-        onKeyUp={live}
-        initialValue=""
-        init={{
-          height: 300,
-          menubar: false,
-          plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount'
-          ],
-          toolbar: 'undo redo | formatselect | ' +
-          'bold italic backcolor | alignleft aligncenter ' +
-          'alignright alignjustify | bullist numlist outdent indent | ' +
-          'removeformat | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-        }}
-      />
-      <div className="documents">
-      <h3>Välj ett dokument att redigera:</h3>
-        < Documents onTitleClick={showText} />
-      </div>
-    </>
-  );
+
+  // login = (e) => {
+  //   e.preventDefault();
+  //         fetch('http://localhost:1337/login', {
+  //             method: 'POST',
+  //             headers: {
+  //                 'Accept': 'application/json',
+  //                 'Content-Type': 'application/json',
+  //             },
+  //             body: JSON.stringify({
+  //                 email: e.target[0].value,
+  //                 password: e.target[1].value,
+  //             })
+  //         })
+  //         .then( function (response) {
+  //           return response.json();
+
+  //         }).then(function(data) {
+  //           console.log(data.data.token);
+  //           this.setState({token: data.data.token});
+  //         })
+  // };
+  // reg = (e) => {
+  //   e.preventDefault();
+  //         fetch('http://localhost:1337/register', {
+  //             method: 'POST',
+  //             headers: {
+  //                 'Accept': 'application/json',
+  //                 'Content-Type': 'application/json',
+  //             },
+  //             body: JSON.stringify({
+  //                 email: e.target[0].value,
+  //                 password: e.target[1].value,
+  //             })
+  //         })
+  //         .then( function (response) {
+  //           // console.log(response);
+  //         });
+  // };
+  render() {
+    if (this.state.token == null) {
+      return <FirstPage />
+      // return <FirstPage loginUser={this.login}/>
+    } 
+    return (
+      <>
+      < Toolbar saveText={this.save} logText={this.log} emptyEditor={this.empty} />
+        <Editor
+          id="myTextarea"
+          onInit={(evt, editor) => this.editorRef.current = editor}
+          onKeyUp={this.live}
+          initialValue=""
+          init={{
+            height: 300,
+            menubar: false,
+            plugins: [
+              'advlist autolink lists link image charmap print preview anchor',
+              'searchreplace visualblocks code fullscreen',
+              'insertdatetime media table paste code help wordcount'
+            ],
+            toolbar: 'undo redo | formatselect | ' +
+            'bold italic backcolor | alignleft aligncenter ' +
+            'alignright alignjustify | bullist numlist outdent indent | ' +
+            'removeformat | help',
+            content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+          }}
+        />
+        <p>Inloggad användare: { this.state.email }</p>
+        <div className="documents">
+        <h3>Välj ett dokument att redigera:</h3>
+          < Documents onTitleClick={this.showText} />
+        </div>
+      </>
+    );
+  }
 }
